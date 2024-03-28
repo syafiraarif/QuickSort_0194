29f7598370601fa549563154feb7af0e520c1547
syairaarif <putrisyafira4444@gmail.com>
2024-03-28 09:21:38 +0700

    menambahkan komentar untuk deklarasi variabel

--- QuickSort_0194/QuickSort_0194.cpp
+++ QuickSort_0194/QuickSort_0194.cpp
@@ -3,10 +3,10 @@
 #include <iostream>
 using namespace std;
 
-
+//array of integers to hold values
 int arr[20];
-int cmp_cout = 0;
-int mov_cout = 0;
+int cmp_cout = 0;			// number of comparasion
+int mov_cout = 0;			// number of data movements
 int n;
 
 int main()

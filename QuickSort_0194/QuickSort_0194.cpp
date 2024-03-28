f61081a9a2d1798804039099a3925cec74536246
syairaarif <putrisyafira4444@gmail.com>
2024-03-28 09:42:40 +0700

    membuat tahapan q_short dari tahap 1 sampai 4

--- QuickSort_0194/QuickSort_0194.cpp
+++ QuickSort_0194/QuickSort_0194.cpp
@@ -43,7 +43,16 @@ void swap(int x, int y)
 
 void q_short ( int low, int high)
 {
+	int temp;
+	int pivot, i, j;
+	if (low > high) 
+	{
+		return;
+	}
 
+	pivot = arr[low];
+	i = low + 1;
+	j = high;
 }
 
 int main()
